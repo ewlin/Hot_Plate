fe975db6810fc8ede29de1b3d52fdfa923ea62d7
Eric <ericwilliamlin@gmail.com>
2015-05-30 19:15:58 -0400

    changed return to self/grid object

--- grid.rb
+++ grid.rb
@@ -1,23 +1,27 @@
 class Grid
     #initialize grid
-    def initialize(gridSize)
-        @gridSize = gridSize
+    def initialize(gridDimension)
+        #declare grid dimensions 
+        @gridSize = gridDimension
         @maxDifference = 0
-        @grid = Array.new(gridSize){[]}
+        
+        #create grid container 
+        @grid = Array.new(gridDimension){[]}
         @turns = 0
-
-        outerCounter = 0
+        
+        #fill grid with values 
+        rowCounter = 0
         @grid.each do |row|
-            innerCounter = 0
-            while innerCounter < @gridSize do
-                if outerCounter == 0 || outerCounter == (@grid.length - 1)
-                    if innerCounter == 0 || innerCounter == @grid.length - 1
+            columnCounter = 0
+            while columnCounter < @gridSize do
+                if rowCounter == 0 || rowCounter == (@grid.length - 1)
+                    if columnCounter == 0 || columnCounter == @grid.length - 1
                         x = 0
                     else
                         x = 50
                     end
-                elsif outerCounter == (@gridSize/2) || outerCounter == (@gridSize/2)-1
-                    if innerCounter == (@gridSize/2) || innerCounter == (@gridSize/2)-1
+                elsif rowCounter == (@gridSize/2) || rowCounter == (@gridSize/2)-1
+                    if columnCounter == (@gridSize/2) || columnCounter == (@gridSize/2)-1
                         x = 100
                     else
                         x = 50
@@ -26,10 +30,11 @@ class Grid
                     x = 50
                 end
                 row.push(x)
-                innerCounter += 1
+                columnCounter += 1
             end
-            outerCounter += 1
+            rowCounter += 1
         end
+        
     end
 
     def hot_plate()
@@ -72,34 +77,8 @@ class Grid
             @turns += 1
             hot_plate()
         else
-            print @grid
-            print @maxDifference
+            return self
         end
     end
 
 end
-
-#if i == 0
-#    if @grid[i][j] == 0
-#        newValue = 0
-#    else
-#        newValue = ((@grid[i][j-1] + @grid[i+1][j] + @grid[i][j+1] + @grid[@gridSize-1][j])/4.to_f)
-#    end
-#elsif i == (@gridSize-1)
-#    if @grid[i][j] == 0
-#        newValue = 0
-#    else
-#        newValue = ((@grid[i][j-1] + @grid[i-1][j] + @grid[i][j+1] + @grid[0][j])/4.to_f)
-#    end
-#else
-#    if @grid[i][j] == 100
-#        newValue = 100
-#    elsif j == 0
-#        newValue = ((@grid[i-1][j] + @grid[i+1][j] + @grid[i][j+1] + @grid[i][@gridSize-1])/4.to_f)
-#    elsif j == (@gridSize-1)
-#        newValue = ((@grid[i-1][j] + @grid[i+1][j] + @grid[i][j-1] + @grid[i][0])/4.to_f)
-#    else
-#        newValue = ((@grid[i-1][j] + @grid[i+1][j] + @grid[i][j-1] + @grid[i][j+1])/4.to_f)
-#    end
-#end
-    
